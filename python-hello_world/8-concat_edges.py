af176a7d02a869776aa46fc2ff04844867f036ec
shahadeissaalharbi <shahadeissa111@gmail.com>
2026-06-06 13:12:12 +0000

    Fix 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 str1 = "Python is an interpreted, interactive, object-oriented programming language that is often compared (or combined) with Perl, Lisp, Java, and Ruby."
-print(str1[39:67] + str1[113:118] + str1[0:6])
+str2 = str1[39:67] + str1[113:118] + str1[0:6]
+print(str2)
