6b80c7e3ed00930512270f32a967e307e519ac19
shahadeissaalharbi <shahadeissa111@gmail.com>
2026-06-06 13:13:32 +0000

    Fix variable name to str

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str1 = "Python is an interpreted, interactive, object-oriented programming language that is often compared (or combined) with Perl, Lisp, Java, and Ruby."
-str2 = str1[39:67] + str1[113:118] + str1[0:6]
-print(str2)
+str = str1[39:67] + str1[113:118] + str1[0:6]
+print(str)
