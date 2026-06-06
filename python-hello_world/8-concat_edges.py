8f3c482ba2f1dcc2027e007e9cf0c164d9f35ea5
shahadeissaalharbi <shahadeissa111@gmail.com>
2026-06-06 14:01:18 +0000

    Fix pycodestyle 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-str1 = "Python is an interpreted, interactive, object-oriented programming lan" \
-"guage that is often compared (or combined) with Perl, Lisp, Java, and Ruby."
+str1 = "Python is an interpreted, interactive, object-oriented programming " \
+    "language that is often compared (or combined) with Perl, Lisp, " \
+    "Java, and Ruby."
 str = str1[39:67] + str1[113:118] + str1[0:6]
 print(str)
