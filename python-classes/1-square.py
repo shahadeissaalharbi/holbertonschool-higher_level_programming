26cecca71faecc4c6d1fae077296184cfc605f52
shahadeissaalharbi <shahadeissa111@gmail.com>
2026-06-20 18:47:58 +0000

    Add 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+
+class Square:
+    """A class that defines a square by its size."""
+
+    def __init__(self, size):
+        self.__size = size
