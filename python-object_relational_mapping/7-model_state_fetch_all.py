536b258116d5f5858053c57ac3239798d24cae76
shahadeissaalharbi <shahadeissa111@gmail.com>
2026-07-22 13:52:27 +0000

    7. All states via SQLAlchemy: Add script to list all State objects ordered by id

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,26 +1,34 @@
 #!/usr/bin/python3
-"""Script that lists all State objects from the database."""
+"""
+Script that lists all State objects from the database hbtn_0e_6_usa,
+using SQLAlchemy.
+
+Usage:
+    ./7-model_state_fetch_all.py <mysql_username> <mysql_password> <db_name>
+"""
 import sys
 from model_state import Base, State
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    """Connect to MySQL and print all states sorted by id."""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            username, password, db_name
         ),
         pool_pre_ping=True
     )
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
     states = session.query(State).order_by(State.id).all()
+
     for state in states:
         print("{}: {}".format(state.id, state.name))
 
